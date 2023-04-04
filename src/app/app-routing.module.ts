e2d150e72ece0878094bef74b8ccaa352b74fef1
Pierre tubert <pierretubertpro@gmail.com>
2023-04-04 09:55:57 +0200

    12

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { EcoloComponent } from './ecolo/ecolo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EcoloComponent },
+  { path: '/inscription', component: RegisterComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
